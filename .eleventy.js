8ae68bc80d1990eb2fedc5ebb6031493963f77e2
Manik Dingra <manikdhingra0582@gmail.com>
2023-08-28 23:50:24 +0530

    Njk is used to create templates created pages for header and footer and article too

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("./src/webfonts");
+    eleventyConfig.addPassthroughCopy("./src/blogs");
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL)
+    });
+    return {
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+};
